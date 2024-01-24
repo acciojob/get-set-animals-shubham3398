666d599456d3049935daa954d7e5dcb71869df71
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2024-01-24 12:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	
+	get getSpecies(){
+		return this.species;
+	}
+	
+	makeSound() {
+		console.log("making sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
 
-class Cat extends Animal {}
+	bark(){
+		console.log("bark");
+	}
+}
+
+class Cat extends Animal {
+	constructor(species);
+
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
